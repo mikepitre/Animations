538d5426f426077c11f8512744b6381e9bcebbec
mikepitre <michaelepitre@gmail.com>
2015-09-24 11:33:24 -0500

    created loop to loop thru frames of animation

--- Animations/ViewController.swift
+++ Animations/ViewController.swift
@@ -9,7 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var counter = 1
 
+    @IBOutlet weak var alienImage: UIImageView!
+    
+    @IBAction func updateImage(sender: AnyObject) {
+        
+        if counter == 5 {
+            
+            counter = 1
+            
+        } else {
+        
+        counter++
+        
+        }
+        
+        alienImage.image = UIImage(named: "frame\(counter).png")
+        
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
